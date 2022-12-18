12af8476b921c7febd5af622dd07a8407783592e
qclaytor30 <qclaytor30@gmail.com>
2022-12-18 13:49:59 -0500

    fix develop folder/tag and product

--- models/Tag.js
+++ models/Tag.js
@@ -7,12 +7,22 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    
+    product_name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
     timestamps: false,
-    freezeTableName: true,
     underscored: true,
+    freezeTableName: true,
     modelName: 'tag',
   }
 );
